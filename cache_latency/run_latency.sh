8a64c58ca9566f832f32c30180d0495d4a927e6d
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-10-06 21:36:34 +0200

    Fix cache latency benchmark

--- cache_latency/run_latency.sh
+++ cache_latency/run_latency.sh
@@ -5,9 +5,9 @@ convert_to_bytes() {
     unit=$2
 
     case $unit in
-        KiB) echo $(($size * 1024)) ;;
-        MiB) echo $(($size * 1024 * 1024)) ;;
-        GiB) echo $(($size * 1024 * 1024 * 1024)) ;;
+        KiB) echo $(echo "$size * 1024" | bc) ;;
+        MiB) echo $(echo "$size * 1024 * 1024" | bc) ;;
+        GiB) echo $(echo "$size * 1024 * 1024 * 1024" | bc) ;;
         *) echo $size ;;
     esac
 }
@@ -25,8 +25,6 @@ l3_bytes=$(convert_to_bytes $(echo $l3))
 cache_sizes=("$l1d_bytes" "$l2_bytes" "$l3_bytes")
 threads=(1 2 4 6 8 12 16 18 20 22 24 28 32 36 40 44 48)
 
-echo "${cache_sizes[@]}"
-
 repeat=100
 ool=100
 for cache_size in "${cache_sizes[@]}";
@@ -34,7 +32,6 @@ do
   input_size=($(echo "$cache_size * 0.8 / 8" | bc))
   for t_count in "${threads[@]}";
   do
-    echo "$input_size $t_count"
     ./latency $t_count $input_size $repeat $ool >> results.txt
   done
 done
