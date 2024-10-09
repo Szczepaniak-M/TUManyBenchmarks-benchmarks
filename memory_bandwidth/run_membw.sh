afee2ad6c68d96352b4c1d590b8a01a52c21ebcd
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-10-09 23:01:28 +0200

    Fix memory bandwidth benchmark for CPUs with multiple NUMA nodes

--- memory_bandwidth/run_membw.sh
+++ memory_bandwidth/run_membw.sh
@@ -2,9 +2,10 @@
 
 echo "threads,bw,sum" > results.csv
 threads=(1 2 4 6 8 12 16 18 20 22 24 28 32 36 40 44 48) 
-# fill 70% of RAM size with uint64_t
+# fill 90% of RAM available per NUMA node with uint64_t
 size=$(free -b | grep Mem: | awk '{print $2}')
-input_size=$(echo "$size * 0.7 / 8" | bc)
+numa_nodes=$(lscpu | grep "NUMA node(s):" | awk '{print $3}')
+input_size=$(echo "$size / $numa_nodes * 0.9 / 8" | bc)
 
 for t_count in "${threads[@]}";
 do
