52965573961462d7529d083efa9151d0a1f5ca31
Till Steinert <till.steinert@tum.de>
2024-10-17 16:02:02 +0200

    other benchmarks should run on ssd_size size

--- ssd_bandwidth/run_fio.sh
+++ ssd_bandwidth/run_fio.sh
@@ -26,5 +26,5 @@ experiments=("randwrite" "read" "randread")
 for exp in "${experiments[@]}"
 do
     output_file="results-${exp}.json"
-    sudo fio --name=readio --rw="${exp}" --numjobs=16 --refill_buffers --ioengine=libaio --time_based --runtime=60s --group_reporting --iodepth=128 --bs=4k --filename=/dev/nvme1n1 --size=20G --direct=1 --output-format=json --output="$output_file"
+    sudo fio --name=readio --rw="${exp}" --numjobs=16 --refill_buffers --ioengine=libaio --time_based --runtime=60s --group_reporting --iodepth=128 --bs=4k --filename=/dev/nvme1n1 --size="$ssd_size"G --direct=1 --output-format=json --output="$output_file"
 done
