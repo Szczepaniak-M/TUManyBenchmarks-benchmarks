823c739fab30dbb092718f8e07aaef1024871d7e
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-10-26 15:39:54 +0200

    Add SPEC CPU all cores

---
+++ spec_cpu_all_cores/run_spec.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+arch=$(lscpu | grep "Architecture" | awk '{print $2}')
+cores=$(( $(lscpu | grep "^CPU(s):" | awk '{print $2}') - 1 ))
+
+cd /home/ubuntu/specbench
+source shrc
+if [[ "$arch" == "x86_64" ]]; then
+    runcpu --config=config_x86 --iterations=2 --copies=$cores --reportable intrate >> log.txt
+elif [[ "$arch" == "aarch64" ]]; then
+    runcpu --config=config_graviton --iterations=2 --copies=$cores --reportable intrate >> log.txt
+fi
