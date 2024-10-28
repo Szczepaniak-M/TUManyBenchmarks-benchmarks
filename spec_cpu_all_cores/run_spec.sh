4391f7fc9da4ed43ef868a6df164b0013111a2c7
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-10-28 16:03:12 +0100

    Try to use all cores for SPEC CPU

--- spec_cpu_all_cores/run_spec.sh
+++ spec_cpu_all_cores/run_spec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 arch=$(lscpu | grep "Architecture" | awk '{print $2}')
-cores=$(( $(lscpu | grep "^CPU(s):" | awk '{print $2}') - 1 ))
+cores=$(( $(lscpu | grep "^CPU(s):" | awk '{print $2}') ))
 
 cd /home/ubuntu/specbench
 source shrc
